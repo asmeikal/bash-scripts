e4de55bcbf315d258c3eb4067aac6d19e81eaca0
Michele Laurenti <asmeikal@me.com>
2016-02-09 23:14:29 +0100

    Add touchpad enable/disable and underclock script

--- touchpad.sh
+++ touchpad.sh
@@ -3,6 +3,12 @@
 TOUCHPAD="SynPS/2 Synaptics TouchPad"
 PROPERTY="Device Enabled"
 
+usage() {
+	SCRIPT_NAME=`echo $0 | sed -r -e "s/.*\///"`
+	echo "Usage:$SCRIPT_NAME [on|off]"
+	echo "With no argument toggles touchpad."
+}
+
 set_touchpad() {
 	xinput --set-prop "$TOUCHPAD" "$PROPERTY" $1
 }
@@ -20,12 +26,20 @@ toggle_touchpad() {
 	esac
 }
 
+if [[ $# > 1 ]] ; then
+	usage
+	exit 1
+fi
+
 case $1 in
 	"on") set_touchpad 1
 		;;
 	"off") set_touchpad 0
 		;;
-	*) toggle_touchpad
+	"") toggle_touchpad
+		;;
+	*) usage
+		exit 1
 		;;
 esac
 
--- underclock.sh
+++ underclock.sh
@@ -3,7 +3,12 @@
 LOW_FREQ="1.6GhZ"
 GOVERNOR="powersave"
 NPROC=`nproc`
-echo "We have $NPROC processors."
+
+usage() {
+	SCRIPT_NAME=`echo $0 | sed -r -e "s/.*\///"`
+	echo "Usage:$SCRIPT_NAME [print]"
+	echo "With no argument underclocks CPU."
+}
 
 underclock() {
 	for i in `seq 0 $(( $NPROC - 1))`; do
@@ -27,10 +32,18 @@ printstatus() {
 	done
 }
 
+if [[ $# > 1 ]] ; then
+	usage
+	exit 1
+fi
+
 case $1 in
 	"print") printstatus
 		;;
-	*) underclock
+	"") underclock
+		;;
+	*) usage
+		exit 1
 		;;
 esac
 
